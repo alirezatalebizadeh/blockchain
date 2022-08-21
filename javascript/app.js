464d5a2bbd1916994ffe8d823a907c0537c3ad45
alireza talebizadeh <btc.lider78@gmail.com>
2022-08-21 05:33:04 +0430

    This is version 1 of the website, which includes the header and the section related to photo

---
+++ javascript/app.js
@@ -0,0 +1,6 @@
+// let elem = document.getElementById("animation");
+// let max_width = document.max_width;
+// if (max_width <= 99) {
+//     elem.classList.remove("animation");
+//     elem.classList.remove("start-home");
+// }
