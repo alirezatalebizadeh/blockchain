7046776a16733f80b04a4449ad36cbea7be7541c
alireza talebizadeh <btc.lider78@gmail.com>
2022-08-22 05:50:05 +0430

    Version 3 of the website where the footer section has been added

--- javascript/app.js
+++
@@ -1,6 +0,0 @@
-// let elem = document.getElementById("animation");
-// let max_width = document.max_width;
-// if (max_width <= 99) {
-//     elem.classList.remove("animation");
-//     elem.classList.remove("start-home");
-// }
